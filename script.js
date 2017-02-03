ad8978a73557e4329f84af9efc018536ae7c0385
Jonny Holupchinski <jonholup@gmail.com>
2017-02-03 16:33:44 -0600

    fixed button, eliminated line breaks

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $("button").click(function(){
     $("#container").toggle();
+    $(this).text(function(i, v){
+   return v === 'Press On' ? 'Press Off' : 'Press On'
+})
 });
 });
